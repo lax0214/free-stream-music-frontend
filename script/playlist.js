298469a536a226d3e9b7d4590c4cd4edf8fa9a7e
Bruce <liupengfei02@shouqianba.com>
2024-04-07 22:36:41 +0800

    to: playlist checkbox

--- script/playlist.js
+++ script/playlist.js
@@ -1,5 +1,44 @@
-const domain = "http://127.0.0.1:5500"; 
+const domain = "http://127.0.0.1:5500";
+var checkboxNum = {
+    total: 0,
+    checkedNum: 0
+}
 window.onload = renderPlaylist();
+
+
+var selectAllCheckbox = document.getElementById("select-all-checkbox");
+var exportButton = document.getElementById("button-export");
+// 变更全选和导出按钮状态 
+let checkbox = document.getElementsByClassName("playlist-item-checkbox");
+let coverImg = document.getElementsByClassName("cover-img");
+for (let i = 0; i < checkbox.length; i++) {
+    checkbox[i].addEventListener("change", () => changePlaylistStatus(checkbox[i]));
+}
+for (let i = 0; i < coverImg.length; i++) {
+    coverImg[i].addEventListener("click", () => {
+        var checkbox = coverImg[i].previousElementSibling;;
+        checkbox.checked = true;
+        changePlaylistStatus(checkbox);
+    });
+}
+
+function changePlaylistStatus(checkbox) {
+    if (checkbox.checked) {
+        checkboxNum.checkedNum++;
+    } else {
+        checkboxNum.checkedNum--;
+    }
+    if (checkboxNum.checkedNum > 0) {
+        exportButton.disabled = false;
+        if (checkboxNum.checkedNum == checkboxNum.total) {
+            selectAllCheckbox.checked = true;
+        } else {
+
+        }
+    } else {
+        exportButton.disabled = true;
+    }
+}
 
 function renderPlaylist() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -17,31 +56,32 @@ function renderPlaylist() {
         uid: uid
     };
     console.log(uid);
+    checkboxNum.total = 12;
 
-    const userInfo = document.getElementById("user-info");
-    userInfo.remove();
-    fetch(domain, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                // 查询失败
-                throw new Error('网络错误');
-            }
-        }) // 解析JSON响应
-        .then(data => {
-            // TODO 如果数据不为空则渲染页面，否则提示用户数据为空
-            console.log(data)
-        }) // 处理数据
-        .catch(error => {
-            // 恢复删除的div
-            document.body.appendChild(userInfo);
-            console.error('错误:', error);
-        });
+    // const userInfo = document.getElementById("user-info");
+    // userInfo.remove();
+    // fetch(domain, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(requestBody)
+    // })
+    //     .then(response => {
+    //         if (response.ok) {
+    //             return response.json();
+    //         } else {
+    //             // 查询失败
+    //             throw new Error('网络错误');
+    //         }
+    //     }) // 解析JSON响应
+    //     .then(data => {
+    //         // TODO 如果数据不为空则渲染页面，否则提示用户数据为空
+    //         console.log(data)
+    //     }) // 处理数据
+    //     .catch(error => {
+    //         // 恢复删除的div
+    //         document.body.appendChild(userInfo);
+    //         console.error('错误:', error);
+    //     });
 }
